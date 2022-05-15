d721cfee409489802018598b75d1684dbc3b08a6
rezazare <programmer1376@gmail.com>
2022-05-14 22:06:57 -0700

    first way

---
+++ src/Element/Element.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Element(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: props.color,
+        padding: "50px",
+        borderRadius: "10px",
+      }}
+    >
+      {" "}
+    </div>
+  );
+}
+
+export default Element;
